04dfcadaf0de67c0f90dca2d3525c61027e1727f
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2016-10-11 15:23:04 -0500

    Sound Testing

---
+++ Assets/Scripts/Listener.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Listener : MonoBehaviour {
+
+    bool hearingNoise;
+    List<AudioSource> audioSources;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    List<AudioSource> FindSounds()
+    {
+        List<AudioSource> audio = new  List<AudioSource>();
+        foreach (AudioSource a in FindObjectsOfType<AudioSource>())
+        {
+            if (a.isPlaying)
+            {
+                if((transform.position - a.transform.position).sqrMagnitude <= a.maxDistance)
+                audio.Add(a);
+            }
+                
+        }
+
+        return audio;
+    }
+}
